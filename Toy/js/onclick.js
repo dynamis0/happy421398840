ab8a2aebd0cb803e67e073c10ac6fd93b3c37d0c
Dynamis <happy421398840@gmail.com>
2020-01-31 00:57:44 +0900

    just a toy
    just a toy

---
+++ Toy/js/onclick.js
@@ -0,0 +1,35 @@
+function firstText()
+{
+	if(document.getElementById("name").className == "nameclose"){
+		document.getElementById("name").className="nameopne";
+	}else{
+		document.getElementById("name").className="nameclose";
+	}
+}
+
+function secendText()
+{
+	if(document.getElementById("skill").className == "skillclose"){
+		document.getElementById("skill").className="skillopne";
+	}else{
+		document.getElementById("skill").className="skillclose";
+	}
+}
+
+function thirdText()
+{
+	if(document.getElementById("carrer").className == "carrerclose"){
+		document.getElementById("carrer").className="carreropne";
+	}else{
+		document.getElementById("carrer").className="carrerclose";
+	}
+}
+
+function fourthText()
+{
+	if(document.getElementById("birthday").className == "birthdayclose"){
+		document.getElementById("birthday").className="birthdayopne";
+	}else{
+		document.getElementById("birthday").className="birthdayclose";
+	}
+}
